d186f3306617e37cc843392a6862a19bb2c9fcdb
Michael Brindle <you@subluminal.net>
2012-05-17 20:26:54 +0000

    getting closer to figuring out why this doesn't work.

--- send.c
+++ send.c
@@ -14,20 +14,26 @@
 #include <arpa/inet.h>
 #include <errno.h>
 
-void assemblePacket(char *packet, char *sha, char *spa, char *tha, char *tpa);
+void assemblePacket(unsigned char *packet, unsigned char *sha, unsigned char *spa, unsigned char *tha, unsigned char *tpa);
 addresses *getAddresses(char *dev);
 
 void sendPackets(int sockfd, addresses *ourAddresses, int targetIP)
 {
     // target is in host byte order.
     // We need to convert it to network byte order.
-    char sha[6], spa[4], tha[6], tpa[4];
+    unsigned char sha[6], spa[4], tha[6], tpa[4];
     int targetNetwork = htonl(targetIP);
-    sprintf(tha, "%08x", 0xffffffff);
-    memcpy(sha, ourAddresses->mac, 6);
-    memcpy(spa, ourAddresses->ip, 4);
+    for (int i = 0; i < 6; i++) {
+        tha[i] = 0xff;
+        sha[i] = ourAddresses->mac[i];
+    }
+    for (int i = 0; i < 4; i++) {
+        spa[i] = ourAddresses->ip[i];
+    }
+    //memcpy(sha, ourAddresses->mac, 6);
+    //memcpy(spa, ourAddresses->ip, 4);
     memcpy(tpa, &targetNetwork, 4);
-    char *packet = (char *)calloc(1, 40);
+    unsigned char *packet = (unsigned char *)calloc(1, 40);
     assemblePacket(packet, sha, spa, tha, tpa);
     write(sockfd, packet, 40);
     sync();
@@ -50,24 +56,31 @@ addresses *getAddresses(char *dev)
     device = alldevs;
     pcap_addr_t list, *a;
     while (device != NULL) {
-        if (strncmp(device->name, dev, strlen(dev)) == 0) {
+        if (strncmp(device->name, dev, strlen(device->name)) == 0) {
             // found the device we're looking for.
             list = device->addresses[0];
             for (a = device->addresses; a != NULL; a = a->next) {
                 if (a->addr->sa_family == AF_LINK && a->addr->sa_data != NULL) {
                     link = (struct sockaddr_dl*)a->addr->sa_data;
-                    char mac[link->sdl_alen];
+                    unsigned char mac[link->sdl_alen];
                     caddr_t macaddr = LLADDR(link);
                     memcpy(mac, LLADDR(link), link->sdl_alen);
                     if (link->sdl_alen == 6) {
-                        sprintf((char *)ret->mac, "%02x%02x%02x%02x%02x%02x",(unsigned char)mac[0], (unsigned char)mac[1], (unsigned char)mac[2], (unsigned char)mac[3], (unsigned char)mac[4], (unsigned char)mac[5]);
+                        for (int i = 0; i < 6; i++)
+                            ret->mac[i] = mac[i];
+                        //sprintf((char *)ret->mac, "%02x%02x%02x%02x%02x%02x",(unsigned char)mac[0], (unsigned char)mac[1], (unsigned char)mac[2], (unsigned char)mac[3], (unsigned char)mac[4], (unsigned char)mac[5]);
                     } else if (link->sdl_alen > 6) {
-                        sprintf((char *)ret->mac, "%02x%02x%02x%02x%02x%02x",(unsigned char)mac[1], (unsigned char)mac[2], (unsigned char)mac[3], (unsigned char)mac[4], (unsigned char)mac[5], (unsigned char)mac[6]);
+                        for (int i = 0; i < 6; i++)
+                            ret->mac[i] = mac[i+1];
+                        //sprintf((char *)ret->mac, "%02x%02x%02x%02x%02x%02x",(unsigned char)mac[1], (unsigned char)mac[2], (unsigned char)mac[3], (unsigned char)mac[4], (unsigned char)mac[5], (unsigned char)mac[6]);
                     }
+                    list = a[0];
+                    ip = htonl((int)list.addr);
+                    for (int i = 0; i < 4; i++)
+                        ret->ip[i] = (ip >> i) & 0xFF;
+                    break;
                 }
             }
-            ip = htonl((int)list.addr);
-            memcpy(ret->ip, &ip, 4);
             break;
         }
         device = device->next;
@@ -75,7 +88,7 @@ addresses *getAddresses(char *dev)
     return ret;
 }
 
-void assemblePacket(char *packet, char *sha, char *spa, char *tha, char *tpa)
+void assemblePacket(unsigned char *packet, unsigned char *sha, unsigned char *spa, unsigned char *tha, unsigned char *tpa)
 {
     // hey, this ain't working.
     memcpy(packet, tha, 6);
@@ -93,7 +106,19 @@ void assemblePacket(char *packet, char *sha, char *spa, char *tha, char *tpa)
     memcpy((packet+30), tha, 6);
     memcpy((packet+36), tpa, 4);
     fprintf(stderr, "DEBUG: packet is:\n\t");
-    for (int i = 0; i < 42; i++) {
+    for (int i = 0; i < 40; i++) {
+        if (i == 0 || i == 6)
+            fprintf(stderr, " tha:");
+        if (i == 12)
+            fprintf(stderr, " arp:");
+        if (i == 20)
+            fprintf(stderr, " sha:");
+        if (i == 26)
+            fprintf(stderr, " spa:");
+        if (i == 30)
+            fprintf(stderr, " tha:");
+        if (i == 36)
+            fprintf(stderr, " tpa:");
         fprintf(stderr, "%02x:", packet[i]);
     }
     fprintf(stderr, "\n");
